c010cfaf85d6b7df31a548acdd7dc45a8313072c
plainbanana <s1820650@s.tsukuba.ac.jp>
2019-05-10 16:14:37 +0900

    first release

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 //% weight=70 icon="\uf0e7" color=#d2691e block="STEM"
 namespace stem {
-    //% blockId=magnet_switch block="磁気センサ"
+    //% blockId=magnet_switch block="磁気を検知"
     export function readMagnetSwitchStatus(): boolean {
-        if (pins.digitalReadPin(DigitalPin.P0) == 1) {
-            return true;
-        } else {
+        pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
+        if (pins.digitalReadPin(DigitalPin.P0)) {
             return false;
+        } else {
+            return true;
         }
     }
 
@@ -16,12 +17,18 @@ namespace stem {
 
     // calc temperature read from TFW-TP1; TFabWorks Temperature Sensor
     function calcTemperature(rawVal: number): number {
-        const B = 3380.001
-        const R0 = 10000.001
-        const T0 = 25 + 273.15
-        const volt = rawVal / 1024 * 3
-        const R = (0 - (10000 * volt - 30000)) / volt
-        const temp = B * T0 / (Math.log(R / R0) * T0 + B)
+        let temp = 0
+        let T0 = 0
+        let R = 0
+        let R0 = 0
+        let volt = 0
+        let B = 0
+        B = 3380.001
+        R0 = 10000.001
+        T0 = 25 + 273.15
+        volt = rawVal / 1024 * 3
+        R = (0 - (10000 * volt - 30000)) / volt
+        temp = B * T0 / (Math.log(R / R0) * T0 + B)
         // temp = (- 0.05140683 * R ^ 3 - 0.28713975 * R ^ 2 +
         // 40.64780 * R + 143.3983) / (9.579041e-4 * R ^ 3 +
         // 0.08219358 * R ^ 2 + 0.8617257 * R + 1)
