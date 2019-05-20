aa06ee80334f5358880299984d40fe486ed431ad
Your Name <you@example.com>
2019-05-20 16:06:30 +0900

    change order

--- main.ts
+++ main.ts
@@ -9,6 +9,14 @@ namespace stem {
             return false;
     }
 
+    //% blockId=is_dark block="暗い"
+    export function isDark(): boolean {
+        if (input.lightLevel() < 30)
+            return true;
+        else
+            return false;
+    }
+
     //% blockId=turn_on block="スイッチON"
     export function turnON(): void {
         pins.digitalWritePin(DigitalPin.P1, 1)
@@ -19,13 +27,7 @@ namespace stem {
         pins.digitalWritePin(DigitalPin.P1, 0)
     }
 
-    //% blockId=is_dark block="暗い"
-    export function isDark(): boolean {
-        if (input.lightLevel() < 30)
-            return true;
-        else
-            return false;
-    }
+
 
     const ビットパターン_ON  = [1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0]
     const ビットパターン_OFF = [1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0]
