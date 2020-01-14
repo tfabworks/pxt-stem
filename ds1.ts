6a1c59c59092be64c15a53ca3a11f0926b61db33
atsushi-morita <47807023+atsushi-morita@users.noreply.github.com>
2020-01-14 14:55:48 +0900

    stabilized calculate distance
    stabilized calculate distance with the measuring limit and the median filter.

--- ds1.ts
+++ ds1.ts
@@ -7,14 +7,37 @@ namespace stem {
     //% block="Distance[cm] || %format"
     //% group="DS1"
     export function getDistance(format:OutputNumberFormat = OutputNumberFormat.INTEGER):number {
-        pins.digitalWritePin(DigitalPin.P0, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(DigitalPin.P0, 1);
-        control.waitMicros(20);
-        pins.digitalWritePin(DigitalPin.P0, 0);
-        if ( format === OutputNumberFormat.INTEGER ){
-            return Math.round(pins.pulseIn(DigitalPin.P1, PulseValue.High) * 153 / 29 / 2 / 100);
+        // calculate distance -> median filter -> return value
+        let arr: number[] = [];
+        let l;
+        for(l = 0; l < 5; l++) {
+            let pulseWidth;
+            const MAX_DIST_CM = 300;
+            pins.digitalWritePin(DigitalPin.P0, 0);
+            control.waitMicros(60000);
+            pins.digitalWritePin(DigitalPin.P0, 1);
+            control.waitMicros(20);
+            pins.digitalWritePin(DigitalPin.P0, 0);
+            pulseWidth = pins.pulseIn(DigitalPin.P1, PulseValue.High);
+
+            let distance_cm;
+            if (pulseWidth == 0) {
+                distance_cm = MAX_DIST_CM;
+            }
+            else if (pulseWidth >= MAX_DIST_CM / 153 * 29 * 2 * 100) {
+                distance_cm = MAX_DIST_CM;
+            }
+            else{
+                distance_cm = pulseWidth * 153 / 29 / 2 / 100;
+            }
+
+            if ( format === OutputNumberFormat.INTEGER ){
+                distance_cm = Math.round( distance_cm );
+            }
+
+            arr.push(distance_cm);
         }
-        return pins.pulseIn(DigitalPin.P1, PulseValue.High) * 153 / 29 / 2 / 100;
+        arr.sort((n1, n2) => n1 - n2);
+        return arr[2];
     }
 }
