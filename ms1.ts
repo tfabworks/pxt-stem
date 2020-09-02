1c474a0bf31437d1f6bd54cf4fdcca355ca4fe8b
atsushi-morita <47807023+atsushi-morita@users.noreply.github.com>
2020-09-02 12:12:50 +0900

    磁石が離れたら　に変更

--- ms1.ts
+++ ms1.ts
@@ -1,13 +1,13 @@
 namespace stem {
     /**
-     * TFW-MS1が磁石を検知しているとき真を返します。
+     * TFW-MS1から磁石が離れているとき真を返します。
      */
-    //% blockId=MS1_magnetDetect block="磁石が近づいた"
+    //% blockId=MS1_magnetDetouched block="磁石が離れた"
     //% group="MS1"
     //% weight=100
-    export function MS1_magnetDetect(): boolean {
+    export function MS1_magnetDetouched(): boolean {
         pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
-        if (pins.digitalReadPin(DigitalPin.P15) == 0) {
+        if (pins.digitalReadPin(DigitalPin.P15) == 1) {
             return true;
         } else {
             return false;
